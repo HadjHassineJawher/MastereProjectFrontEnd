c2b8e07dfb8b00a1e775ec096157bdeb5d8afc49
manelk <manelkacem11@gmail.com>
2021-12-02 13:48:43 +0100

    Deleting non used files!

--- web-event-project/src/router/index.js
+++ web-event-project/src/router/index.js
@@ -1,20 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [{
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/welcome',
-    name: 'Welcome',
-    component: () => import( /* webpackChunkName: "about" */ '../components/welcome.component.vue')
-  }
-]
+  path: '/',
+  name: 'Welcome',
+  component: () => import( /* webpackChunkName: "about" */ '../components/welcome.component.vue')
+}]
 
 const router = new VueRouter({
   mode: 'history',
