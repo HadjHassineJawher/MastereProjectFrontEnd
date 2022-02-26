b289da2c63062d02f0c7fbffb169895a69b56173
AzizHannafi <hannafimedaziz@gmail.com>
2022-02-26 13:39:41 +0100

    fixing stream and chat

--- src/components/meet.component.vue
+++ src/components/meet.component.vue
@@ -49,8 +49,8 @@ class="fill-height">
 import io from 'socket.io-client';
 import userMixin from "../mixins/user.mixin";
 import Chat from './chat.component.vue'
-const socket = io.connect("http://localhost:3031");
-//const socket = io.connect("https://mpdam-stream-server.herokuapp.com/");
+//const socket = io.connect("http://localhost:3031");
+const socket = io.connect("https://mpdam-stream-server.herokuapp.com/");
 
 
 const myVideo= document.createElement('video')
